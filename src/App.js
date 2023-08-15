3e3607edbd5a3f16dfdf18a4d051a12eced0f1d7
jang95 <81616561+jang95@users.noreply.github.com>
2023-08-15 13:34:31 +0900

    Minor: App에 있던 AuthProvider와 BrowserRouter를 index로 옮김

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import {AuthProvider} from './store/AuthContext';
-import {BrowserRouter} from 'react-router-dom';
 import ScrollToTop from './pages/util/scrollTop';
 import Routing from "./routes/Routing";
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
+import { Fragment } from "react";
 import './css/App.css'
 
 const App = () => {
     return (
-        <AuthProvider>
-            <BrowserRouter>
-                <ScrollToTop/>
-                <div className='App'>
+        <Fragment>
+            <ScrollToTop/>
+            <div className='App'>
                 <Header/>
                 <Routing/>
                 <Footer/>
-                </div>
-            </BrowserRouter>
-        </AuthProvider>
+            </div>
+        </Fragment>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {AuthProvider} from './store/AuthContext';
+import {BrowserRouter} from 'react-router-dom';
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <AuthProvider>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </AuthProvider>
   </React.StrictMode>
 );
