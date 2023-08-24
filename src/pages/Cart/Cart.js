9f87b902a8463d017f7a270ac525084769815b42
jang95 <81616561+jang95@users.noreply.github.com>
2023-08-24 16:55:50 +0900

    FEAT: 장바구니 수정,제거 기능 완료

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -9,10 +9,12 @@ const Cart = () => {
     const [cartData, setCartData] = useState([]);
     const [cartObj, setCartObj] = useState([]);
     const [currentPage, setCurrentPage] = useState(0);
+    const [countList, setCountList] = useState([]);
 
     useEffect(() => {
         getCartList();
-    }, [])
+    }, []);
+
     const getCartList = async (newPage) => {
         try {
             let response = {};
@@ -23,21 +25,43 @@ const Cart = () => {
             if(!newPage) {
                 response = await axios.get('/cart/list');
             }
-
             console.log('response', response);
             const data = response.data;
             setCartObj(data[0]);
             setCartData(data[0].content);
+            const itemCount = data[0].content.map((cartItem) => cartItem.count);
+            setCountList(itemCount);
         } catch (e) {
             console.log('장바구니 목록 조회 오류', e);
         }
     }
 
-    const cartCancel = async (orderId) => {
+    const cartCancel = async (cartItemId) => {
+        const formData = {
+            cartItemId : cartItemId
+        }
         try {
-            const response = await axios.get(`/cart/list`);
+            const response = await axios.post(`/cart/delete`, formData);
+            console.log('장바구니 상품 삭제 성공', response);
+            alert('장바구니 상품 삭제 성공');
+            getCartList();
         } catch (e) {
-            console.log('장바구니 페이지네이션 불러오기 실패', e);
+            console.log('장바구니 상품 삭제 실패', e);
+        }
+    }
+
+    const cartUpdate = async (cartItemId, index) => {
+        const formData = {
+            cartItemId : cartItemId,
+            count : countList[index],
+        }
+        try {
+            const response = await axios.post(`/cart/update`, formData);
+            console.log('장바구니 상품 수정 성공', response);
+            alert('장바구니 수정 성공');
+            getCartList();
+        } catch (e) {
+            console.log('장바구니 상품 삭제 실패', e);
         }
     }
 
@@ -51,7 +75,7 @@ const Cart = () => {
             {
                 cartData !== null ? (
                     <Fragment>
-                        <CartList cartData={cartData}/>
+                        <CartList cartData={cartData} setCartData={setCartData} countList={countList} setCountList={setCountList} cartCancel={cartCancel} cartUpdate={cartUpdate}/>
                         <ItemPagination totalPages={cartObj.totalPages} currentPage={currentPage} onPageChange={handlePageChange}/>
                     </Fragment>
                 ) : (
--- src/pages/Cart/CartList.js
+++ src/pages/Cart/CartList.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
+import {useNavigate} from "react-router-dom";
 import {Table, Button} from 'react-bootstrap';
 import './CartList.css';
 
-const CartList = ({cartData}) => {
+const CartList = ({cartData, countList, setCountList, cartCancel, cartUpdate}) => {
+    const navigate = useNavigate();
+
+    const navigateToProductDetail = useCallback((itemNumber) => {
+        navigate(`/admin/item/detail/${itemNumber}`);
+    }, [navigate]);
+
+    const handleChangeCount = (e, index) => {
+        const updateCartCount = [...countList];
+        updateCartCount[index] = e.target.value;
+        setCountList(updateCartCount);
+    }
 
     return (
         <div className='cart-list-container'>
@@ -10,34 +22,63 @@ const CartList = ({cartData}) => {
                 <thead>
                 <tr>
                     <td className='cart-list-td'>
-                        <input type="checkbox" id='checkAll' />전체 선택
+                        <label className='cart-list-label' htmlFor="checkAll" >전체 선택</label>
+                        <input type="checkbox" id='checkAll' />
                     </td>
                     <td className='cart-list-td'>상품정보</td>
                     <td className='cart-list-td'>상품금액</td>
+                    <td className='cart-list-td'>기능</td>
                 </tr>
                 </thead>
                 <tbody>
                 {
-                    cartData.map(item => (
-                        <tr key={item.itemId}>
+                    cartData.map((cartItem, index) => (
+                        <tr key={cartItem.cartItemId}>
                             <td>
                                 <div className='cart-list-item-checkBox'>
-                                    <input type='checkbox' value={item.itemId}/>
+                                    <input type='checkbox' value={cartItem.cartItemId}/>
                                 </div>
                             </td>
                             <td>
                                 <div className='cart-item-warp'>
-                                <img className='cart-list-item-img' src={item.imgUrl} alt="장바구니 상품 이미지"/>
+                                <img className='cart-list-item-img' src={cartItem.imgUrl} alt="장바구니 상품 이미지"/>
                                 <div className='order-item-info'>
-                                    <h3>{item.itemNm}</h3>
-                                    <span>수량: {item.count}</span>
-                                    <span>가격: {item.price}</span>
+                                    <h3>{cartItem.itemNm}</h3>
+                                    <label className='cart-item-label' htmlFor="cart-item-count">수량</label>
+                                    <input
+                                        id='cart-item-count'
+                                        type='number'
+                                        min={1}
+                                        value={countList[index]}
+                                        onChange={(e) => handleChangeCount(e, index)}/>
                                 </div>
                             </div>
                             </td>
                             <td>
                                 <div className='cart-list-item-total-price'>
-                                    {item.price}
+                                    {cartItem.price * countList[index]}
+                                </div>
+                            </td>
+                            <td>
+                                <div className='cart-list-func'>
+                                    <Button
+                                        variant="success"
+                                        onClick={() => cartUpdate(cartItem.cartItemId, index)}
+                                    >
+                                        수정
+                                    </Button>
+                                    <Button
+                                        variant="danger"
+                                        onClick={() => cartCancel(cartItem.cartItemId)}
+                                    >
+                                        제거
+                                    </Button>
+                                    <Button
+                                        variant="primary"
+                                        onClick={() => navigateToProductDetail(cartItem.cartItemId)}
+                                    >
+                                        상품 보기
+                                    </Button>
                                 </div>
                             </td>
                         </tr>
