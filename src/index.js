c085ba1193101d8d729ab31a973884ad9c9f1f6f
jang95 <81616561+jang95@users.noreply.github.com>
2023-08-30 00:10:47 +0900

    MODIFY: whitelabel error page 해결을 위해 BrowserRouter -> HashRouter로 변경

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {AuthProvider} from './store/AuthContext';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <AuthProvider>
-          <BrowserRouter>
+          <HashRouter>
               <App />
-          </BrowserRouter>
+          </HashRouter>
       </AuthProvider>
   </React.StrictMode>
 );
