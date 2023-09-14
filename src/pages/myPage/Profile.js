6cc5d97a108230d08dbcfd50daf5c0a1039263bf
jang95 <81616561+jang95@users.noreply.github.com>
2023-09-14 12:46:42 +0900

    FEAT: 유저 정보 수정

--- src/pages/myPage/Profile.js
+++ src/pages/myPage/Profile.js
@@ -1,16 +1,52 @@
-import React, {Fragment, useContext, useEffect, useState} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import './Profile.css'
 import {Button} from 'react-bootstrap';
-import AuthContext from "../../store/AuthContext";
+import axios from "axios";
+import {loginItemSetting} from "../../store/Auth";
 
 const Profile = () => {
-    const {user} = useContext(AuthContext);
-
-    const [isEdit, setIsEdit] = useState(false);
+    const [userData, setUserData] = useState({});
+    const [edit, setEdit] = useState(false);
 
     useEffect(() => {
-        console.log('user', user);
+        const user = localStorage.getItem('user');
+        setUserData(JSON.parse(user));
     }, []);
+
+    const profileEdit = async () => {
+        const formData = {
+            phone: userData.phone
+        }
+        try {
+            const response = await axios.post('/member/update', formData);
+            console.log('프로파일 수정 성공', response);
+            await getMemberInfo();
+            window.location.reload();
+        } catch (e) {
+            console.log('프로파일 수정 실패', e);
+        }
+    }
+
+    const getMemberInfo = async () => {
+        try {
+            const response = await axios.get('/member/info');
+            localStorage.setItem('user', JSON.stringify(response.data));
+            setUserData(response.data);
+            loginItemSetting();
+        } catch (e) {
+            console.log('유저 정보 가져오기 오류', e);
+        }
+    }
+
+    const phoneEdit = (e) => {
+        let copyUser = {...userData};
+        copyUser['phone'] = e;
+        setUserData(copyUser);
+    }
+
+    const changeEdit = () => {
+        setEdit(true);
+    }
 
     return (
         <div className='profile-container'>
@@ -18,27 +54,44 @@ const Profile = () => {
             <div className='profile-wrap'>
                 <div className='profile-info'>
                     <div className='profile-info-title'>이름</div>
-                    <div className='profile-info-detail'>해당 내용 들어가있음</div>
+                    <div className='profile-info-detail'>{userData.name}</div>
                 </div>
                 <div className='profile-info'>
                     <div className='profile-info-title'>이메일</div>
-                    <div className='profile-info-detail'>해당 내용 들어가있음</div>
+                    <div className='profile-info-detail'>{userData.email}</div>
                 </div>
                 <div className='profile-info'>
                     <div className='profile-info-title'>번호</div>
-                    <div className='profile-info-detail'>해당 내용 들어가있음</div>
+                    {
+                        edit === true ?
+                            <Fragment>
+                                <input type="tel" value={userData.phone} onChange={(e) => phoneEdit(e.target.value)} />
+                                <Button
+                                    variant="primary"
+                                    type="submit"
+                                    onClick={profileEdit}
+                                >
+                                    저장
+                                </Button>
+                            </Fragment>
+                            :
+                            <Fragment>
+                                <div className='profile-info-detail'>{userData.phone}</div>
+                                <Button
+                                    variant="primary"
+                                    type="submit"
+                                    onClick={changeEdit}
+                                >
+                                    수정
+                                </Button>
+                            </Fragment>
+                    }
                 </div>
                 <div className='profile-info'>
                     <div className='profile-info-title'>등급</div>
-                    <div className='profile-info-detail'>해당 내용 들어가있음</div>
+                    <div className='profile-info-detail'>{userData.grade}</div>
                 </div>
             </div>
-            <Button
-                variant="primary"
-                type="submit"
-            >
-                수정하기
-            </Button>
         </div>
     )
 }
