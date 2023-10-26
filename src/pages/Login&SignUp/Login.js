98a826ec7c534aaa5eb12384bf646b2884c1f0b4
jang95 <81616561+jang95@users.noreply.github.com>
2023-10-26 15:43:38 +0900

    FEAT: 카카오 간편 로그인

--- src/pages/Login&SignUp/Login.js
+++ src/pages/Login&SignUp/Login.js
@@ -1,103 +1,104 @@
-import React, {useState, useContext} from 'react';
-import {useNavigate} from 'react-router-dom';
-import AuthContext from "../../store/AuthContext";
-import {Form, Button, Container, Alert} from 'react-bootstrap';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../store/AuthContext';
+import { loginItemSetting } from '../../store/Auth';
+import { Form, Button, Alert } from 'react-bootstrap';
 import './Login.css';
-import {loginItemSetting} from '../../store/Auth'
-
 import axios from 'axios';
-import NaverLogin from "./NaverLogin";
-import KakaoAuth from "./KakaoAuth";
+import NaverLogin from './NaverLogin';
+import KakaoAuth from './KakaoAuth';
 
 const Login = () => {
-    const { actions} = useContext(AuthContext); // 로그인 상태 관리를 위한 AuthContext 추가
+  const { actions } = useContext(AuthContext); // 로그인 상태 관리를 위한 AuthContext 추가
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLoginSubmit = async (e) => {
-        e.preventDefault();
-
-        const formData = {
-            email: email,
-            password : password,
-        }
-
-        try {
-            const response = await axios.post('/member/login', formData);
-            localStorage.setItem('login-token', JSON.stringify(response.data));
-            actions.setIsLoggedIn(true);
-            actions.setToken(response.data);
-            await getMemberInfo(response.data);
-
-            setError(''); // 에러 메시지 초기화
-            navigate('/'); // 로그인 후 페이지 이동
-        } catch (error) {
-            console.error('Error:', error);
-            setError('로그인하려면 유효한 아이디 또는 비밀번호를 입력하세요.');
-        }
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      email: email,
+      password: password,
     };
 
-    const getMemberInfo = async (token) => {
-        try {
-            const response = await axios.get('/member/info',{
-                headers: {
-                    "Content-Type": "Application/json",
-                    "X-AUTH-TOKEN" : token
-                }
-            });
-            localStorage.setItem('user', JSON.stringify(response.data));
-            loginItemSetting();
-        } catch (e) {
-            console.log('유저 정보 가져오기 오류', e);
-        }
+    try {
+      const response = await axios.post('/member/login', formData);
+      localStorage.setItem('login-token', JSON.stringify(response.data));
+      actions.setIsLoggedIn(true);
+      actions.setToken(response.data);
+      await getMemberInfo(response.data);
+
+      setError(''); // 에러 메시지 초기화
+    } catch (error) {
+      console.error('Error:', error);
+      setError('로그인하려면 유효한 아이디 또는 비밀번호를 입력하세요.');
     }
+  };
 
-    const loginHandler = () => {
-        navigate('/kakao/login');
-    };
+  const getMemberInfo = async (token) => {
+    try {
+      const response = await axios.get('/member/info', {
+        headers: {
+          'Content-Type': 'Application/json',
+          'X-AUTH-TOKEN': token,
+        },
+      });
+      localStorage.setItem('user', JSON.stringify(response.data));
+      localStorage.setItem(
+        'authorities',
+        response.data.authorities[0].authority
+      );
+      actions.setUser(response.data);
+      loginItemSetting();
+      navigate('/'); // 로그인 후 페이지 이동
+    } catch (e) {
+      console.log('유저 정보 가져오기 오류', e);
+    }
+  };
 
-    return (
-        <div className="login-container">
-            <div className="login-wrap">
-                <h2>로그인</h2>
-                {error && <Alert variant="danger">{error}</Alert>}
-                <Form onSubmit={handleLoginSubmit}>
-                    <Form.Group className='login-box'>
-                        <Form.Label>아이디</Form.Label>
-                        <Form.Control
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </Form.Group>
-
-                    <Form.Group className='login-box'>
-                        <Form.Label>비밀번호</Form.Label>
-                        <Form.Control
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        로그인하기
-                    </Button>
-                </Form>
-            </div>
-            <div className='login-other'>
-                <div className='login-platform'>
-                    <KakaoAuth />
-                    <NaverLogin />
-                </div>
-            </div>
+  return (
+    <div className='login-container'>
+      <div className='login-wrap'>
+        <h2>로그인</h2>
+        {error && <Alert variant='danger'>{error}</Alert>}
+        <Form onSubmit={handleLoginSubmit}>
+          <Form.Group className='login-box'>
+            <Form.Label>아이디</Form.Label>
+            <Form.Control
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className='login-box'>
+            <Form.Label>비밀번호</Form.Label>
+            <Form.Control
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Button
+            variant='primary'
+            type='submit'
+          >
+            로그인하기
+          </Button>
+        </Form>
+      </div>
+      <div className='login-other'>
+        <div className='login-platform'>
+          <KakaoAuth />
+          <NaverLogin />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,42 +1,72 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
-import Main from "../pages/main/Main";
-import Login from "../pages/Login&SignUp/Login";
-import SignUp from "../pages/Login&SignUp/SignUp";
-import MyPageMain from "../pages/myPage/MyPageMain";
-import Upload from "../pages/Item/Upload";
-import Edit from "../pages/Item/Edit";
-import ItemManagement from "../pages/Item/ItemManagement";
-import ItemDetail from "../pages/Item/ItemDetail";
-import PrivateRoute from "./PrivateRoute";
-import ProtectRoute from "./ProtectRoute";
-import NaverCallback from "../pages/Login&SignUp/NaverCallback";
-import KakaoCallback from "../pages/Login&SignUp/KakaoCallback";
+import { Routes, Route } from 'react-router-dom';
+import Main from '../pages/main/Main';
+import Login from '../pages/Login&SignUp/Login';
+import SignUp from '../pages/Login&SignUp/SignUp';
+import MyPageMain from '../pages/myPage/MyPageMain';
+import Upload from '../pages/Item/Upload';
+import Edit from '../pages/Item/Edit';
+import ItemManagement from '../pages/Item/ItemManagement';
+import ItemDetail from '../pages/Item/ItemDetail';
+import NaverCallback from '../pages/Login&SignUp/NaverCallback';
+import KakaoCallback from '../pages/Login&SignUp/KakaoCallback';
+import PrivateRoute from './PrivateRoute';
+import ProtectRoute from './ProtectRoute';
 
 const Routing = () => {
-    return (
-        <Routes>
-            {/* 로그인이 아니여도 사용가능 */}
-            <Route path="/" element={<Main/>}/>
-            <Route path="/member/login" element={<Login/>}/>
-            <Route path="/member/signup" element={<SignUp/>}/>
-            <Route path="/admin/item/detail/:itemId" element={<ItemDetail/>} />
-            <Route path="/naverlogin" element={<NaverCallback/>} />
-            <Route path="/kakaoAuth" element={<KakaoCallback/>} />
+  return (
+    <Routes>
+      {/* 로그인이 아니여도 사용가능 */}
+      <Route
+        path='/'
+        element={<Main />}
+      />
+      <Route
+        path='/member/login'
+        element={<Login />}
+      />
+      <Route
+        path='/member/signup'
+        element={<SignUp />}
+      />
+      <Route
+        path='/admin/item/detail/:itemId'
+        element={<ItemDetail />}
+      />
+      <Route
+        path='/naverlogin'
+        element={<NaverCallback />}
+      />
+      <Route
+        path='/kakaoAuth'
+        element={<KakaoCallback />}
+      />
 
-            {/* 로그인 필요 */}
-            <Route element={<PrivateRoute/>}>
-                <Route path="/mypage/*" element={<MyPageMain/>} />
-            </Route>
+      {/* 로그인 필요 */}
+      <Route element={<PrivateRoute />}>
+        <Route
+          path='/mypage/*'
+          element={<MyPageMain />}
+        />
+      </Route>
 
-            {/* 권한 필요 */}
-            <Route element={<ProtectRoute />}>
-                <Route path="/admin/item/new" element={<Upload/>} />
-                <Route path="/admin/item/:itemId" element={<Edit/>} />
-                <Route path="/admin/item" element={<ItemManagement/>} />
-            </Route>
-        </Routes>
-    )
-}
+      {/* 권한 필요 */}
+      <Route element={<ProtectRoute />}>
+        <Route
+          path='/admin/item/new'
+          element={<Upload />}
+        />
+        <Route
+          path='/admin/item/:itemId'
+          element={<Edit />}
+        />
+        <Route
+          path='/admin/item'
+          element={<ItemManagement />}
+        />
+      </Route>
+    </Routes>
+  );
+};
 
 export default Routing;
